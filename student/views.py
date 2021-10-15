e399c60a8fe8d45b9a957ef735674fb3bf0cb824
ByMaglite <fceyhan55@gmail.com>
2021-10-15 21:44:12 +0300

    files updated v4

--- student/views.py
+++ student/views.py
@@ -6,11 +6,12 @@ def index(request):
 
 
 def student_page(request):
+    form = StudentForm()
     if request.method == 'POST':
         form = StudentForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            return redirect('student')  
+            return redirect('/student/')  
     context = {
         'form': form
     }
