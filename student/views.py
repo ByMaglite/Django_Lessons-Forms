f00d74ca03660bbb7894abfa2d4b60fb08ddacad
ByMaglite <fceyhan55@gmail.com>
2021-10-15 21:32:34 +0300

    files updated v3

--- student/views.py
+++ student/views.py
@@ -1,8 +1,17 @@
-from .forms import StudentForm
+from django.shortcuts import render
+from student.forms import StudentForm
+
+def index(request):
+    return render(request, 'student/index.html')
+
 
 def student_page(request):
-    	form = StudentForm()
-    	context = {
-        	'form': form
-    	}
+    if request.method == 'POST':
+        form = StudentForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('student')  
+    context = {
+        'form': form
+    }
     return render(request,'student/student.html', context)
