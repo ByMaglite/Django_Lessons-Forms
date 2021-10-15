0456ccb8d87e2c14f9a3e5b925351c26710cb68d
ByMaglite <fceyhan55@gmail.com>
2021-10-15 21:32:57 +0300

    files updated v3

--- forms/settings.py
+++ forms/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-from decouple import config
 
 SECRET_KEY = config('SECRET_KEY')
 
@@ -127,6 +128,6 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-import os
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
--- student/forms.py
+++ student/forms.py
@@ -1,7 +1,13 @@
 from django import forms
 from .models import Student
 
-class SudentForm(forms.Form):
-    first_name = forms.CharField(max_length=50)
-    last_name = forms.CharField(max_length=50)
-    number = forms.IntegerField(required=False)
+# class StudentForm(forms.Form):
+#     first_name = forms.CharField(max_length=50)
+#     last_name = forms.CharField(max_length=50)
+#     number = forms.IntegerField(required=False)
+
+class StudentForm(forms.ModelForm):
+    class Meta:
+        model = Student
+        fields = ["first_name", "last_name", "number", "profile_pic"]
+        labels = {"first_name": "Name"}
